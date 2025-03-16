8a6a6b5aed97729f0aed9e3bc22eebb04a922771
Memi <165068591+YHidalgoReuinuity@users.noreply.github.com>
2025-03-16 04:24:01 -0500

    added: no items message to product list

--- src/modules/home/components/ProductList/index.tsx
+++ src/modules/home/components/ProductList/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Box } from "@mui/joy";
+import { Box, Typography } from "@mui/joy";
 import { Product } from "../../../shared/types";
 import { ProductCard } from "../ProductCard";
 
@@ -8,6 +8,24 @@ type ProductCardListProps = {
 };
 
 export const ProductList: FC<ProductCardListProps> = (props) => {
+  const { products } = props;
+
+  if (products.length === 0) {
+    return (
+      <Box
+        width="100%"
+        height="100%"
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Typography level="h4" sx={{ color: "text.secondary" }}>
+          No products available.
+        </Typography>
+      </Box>
+    );
+  }
+
   return (
     <Box
       width="100%"
@@ -17,9 +35,9 @@ export const ProductList: FC<ProductCardListProps> = (props) => {
       flexWrap="wrap"
       sx={{ overflowY: "auto" }}
     >
-      {props.products.map((product, index) => {
-        return <ProductCard key={index} product={product} />;
-      })}
+      {products.map((product, index) => (
+        <ProductCard key={index} product={product} />
+      ))}
     </Box>
   );
 };
